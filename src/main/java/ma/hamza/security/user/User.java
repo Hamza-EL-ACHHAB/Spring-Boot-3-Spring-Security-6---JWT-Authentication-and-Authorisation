b671f02c372ac03f49629c761423416f2964ce4e
Hamza-EL-ACHHAB <hamza.elachhab@usmba.ac.ma>
2023-01-21 20:47:50 +0100

    Connection to Postgre and creation of user entity

---
+++ src/main/java/ma/hamza/security/user/User.java
@@ -0,0 +1,61 @@
+package ma.hamza.security.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@Builder /*build object using design pattern builder*/
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+
+public class User implements UserDetails { /*so that ur app user is already a spring user*/
+    @Id
+    @GeneratedValue /*default value is Auto*/
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
